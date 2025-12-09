888e1228d659930c55ac8f6a3c0b9130405b5072
Alexey3883 <aleshazara.543@yandex.ru>
2025-12-09 14:24:46 +1200

    исправил checkStyle

--- ewm-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByAuthorId(Long author);
-    
+
     List<Comment> findAllByEventId(Long eventId);
 }
